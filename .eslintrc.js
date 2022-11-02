d4321e3870ac877e6813cd88ce25f7f38da55c57
Di <novidin@mail.ru>
2022-11-02 12:10:49 +0300

    feat: add solution task 01

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,14 @@
 module.exports = {
   'env': {
     'commonjs': true,
-    'es2022': true,
+    //'es2022': true,
+    'es2021': true,
     'node': true
   },
   'extends': 'eslint:recommended',
   'parserOptions': {
     'ecmaVersion': 12
+    //"ecmaVersion": 2022
   },
   'rules': {
     'indent': [
