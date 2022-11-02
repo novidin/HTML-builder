88290fcfd003d3757b23e8ec29ebf2dbb0893f85
Di <novidin@mail.ru>
2022-11-02 13:53:33 +0300

    feat: add solution task 04

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,21 @@
+const fs = require('fs');
+const path = require('path');
+const srcFolder = path.join(__dirname, 'files/');
+const dstFolder = path.join(__dirname, 'files-copy/');
 
+async function copyDir(src, dst) {
+  await fs.promises.rm(dst, { recursive: true, force: true });
+  await fs.promises.mkdir(dst, { recursive: true });
+  let files = await fs.promises.readdir(src, { withFileTypes: true });
+  for (const file of files) {
+    if (!file.isDirectory()) {
+      const readStream = fs.createReadStream(src + file.name);
+      const writeStream = fs.createWriteStream(dst + file.name);
+      readStream.pipe(writeStream);
+    } else {
+      copyDir(src + file.name + '/', dst + file.name + '/');
+    }
+  }
+}
+
+copyDir(srcFolder, dstFolder);
